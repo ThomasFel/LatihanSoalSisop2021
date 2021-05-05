eff2092947999d957d3d6a424b28c958b12fa8bd
ThomasFelix <37539546+ThomasFel@users.noreply.github.com>
2021-05-05 14:30:18 +0700

    Update soal2.c

--- Modul 2/SOAL 2/soal2.c
+++ Modul 2/SOAL 2/soal2.c
@@ -6,34 +6,34 @@
 #include <time.h>
 
 int main() {
-	pid_t child_id;
-	int status;
+     pid_t child_id;
+     int status;
 
-	time_t rawtime;
-	struct tm *timeinfo;
-	char buffer[80];
+     time_t rawtime;
+     struct tm *timeinfo;
+     char buffer[80];
 
-	time (&rawtime);
-	timeinfo = localtime(&rawtime);
-	strftime(buffer, 80, "%d-%m-%Y_%X", timeinfo);
+     time (&rawtime);
+     timeinfo = localtime(&rawtime);
+     strftime(buffer, 80, "%d-%m-%Y_%X", timeinfo);
 
-	child_id = fork();
+     child_id = fork();
 
-	if (child_id < 0) {
-		exit(EXIT_FAILURE); // Jika gagal membuat proses baru, program akan berhenti
-  	}
+     if (child_id < 0) {
+          exit(EXIT_FAILURE); // Jika gagal membuat proses baru, program akan berhenti
+     }
 
-	if (child_id == 0) {
-    	// this is child
+     if (child_id == 0) {
+     // this is child
 
-		char *argv[] = {"cp", "-r", "/home/thomasfelix/Downloads", "/home/thomasfelix/modul2", NULL};
-		execv("/bin/cp", argv);
-  	}
-  
-	else {
-   	// this is parent
-		while ((wait(&status)) > 0);
-		char *argv[] = {"mv", "modul2", buffer, NULL};
-		execv("/bin/mv", argv);
-	}
+          char *argv[] = {"cp", "-r", "/home/thomasfelix/Downloads", "/home/thomasfelix/modul2", NULL};
+	  execv("/bin/cp", argv);
+     }
+
+     else {
+     // this is parent
+	  while ((wait(&status)) > 0);
+	  char *argv[] = {"mv", "modul2", buffer, NULL};
+	  execv("/bin/mv", argv);
+     }
 }
